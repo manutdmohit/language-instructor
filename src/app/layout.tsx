d3da4d8d90229841a98218e78d03681ae2331775
Mohit Saud <mohit@prabidhilabs.com>
2024-06-07 12:29:13 +0545

    Add a Call Button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import CallButton from '@/components/CallButton';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           <Navbar />
           <main>{children}</main>
           <Footer />
+          <CallButton />
         </div>
       </body>
     </html>
---
+++ src/components/CallButton.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import { FaCommentDots } from 'react-icons/fa';
+
+const number = '+9779868551045';
+
+const CallButton = () => {
+  return (
+    <Link href={`tel:${number}`} className="fixed bottom-5 right-5">
+      <div className="flex items-center justify-center w-16 h-16 bg-green-500 rounded-full shadow-lg transition-transform transform hover:scale-110 animate-pulse">
+        <FaCommentDots className="text-white text-3xl" />
+      </div>
+    </Link>
+  );
+};
+
+export default CallButton;
