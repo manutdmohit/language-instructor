758663acf9580941741916a11715cbd6aa1c421a
Mohit Saud <mohit@prabidhilabs.com>
2024-06-17 06:15:03 +0545

    Fix: call button bug - Remove Link tag and used anchor tag

--- src/components/CallButton.tsx
+++ src/components/CallButton.tsx
@@ -1,15 +1,14 @@
-import Link from 'next/link';
 import { FaCommentDots } from 'react-icons/fa';
 
 const number = '+9779749902198';
 
 const CallButton = () => {
   return (
-    <Link href={`tel:${number}`} className="fixed bottom-5 right-5">
+    <a href={`tel:${number}`} className="fixed bottom-5 right-5">
       <div className="flex items-center justify-center w-16 h-16 bg-green-500 rounded-full shadow-lg transition-transform transform hover:scale-110 animate-pulse">
         <FaCommentDots className="text-white text-3xl" />
       </div>
-    </Link>
+    </a>
   );
 };
 
